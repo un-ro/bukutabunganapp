4c5ff816b879d95985738421aa48a94cb89d52d4
Unero <ramadhanbhg@gmail.com>
2022-07-30 19:28:42 +0700

    - add account function

--- app/src/main/java/dev/unero/bukutabungan/data/repository/AccountRepositoryImpl.kt
+++ app/src/main/java/dev/unero/bukutabungan/data/repository/AccountRepositoryImpl.kt
@@ -1,6 +1,35 @@
 package dev.unero.bukutabungan.data.repository
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import dev.unero.bukutabungan.domain.repository.AccountRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
-class AccountRepositoryImpl: AccountRepository {
+val Context.datastore: DataStore<Preferences> by preferencesDataStore("account")
+class AccountRepositoryImpl(
+    private val context: Context
+): AccountRepository {
+    override suspend fun getUsername(): Flow<String> = context.datastore.data.map { prefs ->
+        prefs[PREF_USERNAME] ?: "user"
+    }
+
+    override suspend fun getPassword(): Flow<String> = context.datastore.data.map { prefs ->
+        prefs[PREF_PASSWORD] ?: "user"
+    }
+
+    override suspend fun setPassword(newPassword: String) {
+        context.datastore.edit { account ->
+            account[PREF_PASSWORD] = newPassword
+        }
+    }
+
+    companion object {
+        private val PREF_USERNAME = stringPreferencesKey("USERNAME")
+        private val PREF_PASSWORD = stringPreferencesKey("PASSWORD")
+    }
 }
--- app/src/main/java/dev/unero/bukutabungan/domain/repository/AccountRepository.kt
+++ app/src/main/java/dev/unero/bukutabungan/domain/repository/AccountRepository.kt
@@ -1,4 +1,9 @@
 package dev.unero.bukutabungan.domain.repository
 
+import kotlinx.coroutines.flow.Flow
+
 interface AccountRepository {
+    suspend fun getUsername(): Flow<String>
+    suspend fun getPassword(): Flow<String>
+    suspend fun setPassword(newPassword: String)
 }
