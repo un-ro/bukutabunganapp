b4aaeda8194e2646ca174ea12eb8e64cb079bf41
Unero <ramadhanbhg@gmail.com>
2022-07-30 16:08:27 +0700

    complete History function

--- app/src/main/java/dev/unero/bukutabungan/ui/history/HistoryFragment.kt
+++ app/src/main/java/dev/unero/bukutabungan/ui/history/HistoryFragment.kt
@@ -7,11 +7,17 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import dev.unero.bukutabungan.databinding.FragmentHistoryBinding
+import dev.unero.bukutabungan.ui.adapter.RecordAdapter
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HistoryFragment : Fragment() {
 
     private var _binding: FragmentHistoryBinding? = null
     private val binding get() = _binding as FragmentHistoryBinding
+
+    private val viewModel: HistoryViewModel by viewModel()
+
+    private val adapter = RecordAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +30,13 @@ class HistoryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.toolbar.setNavigationOnClickListener { findNavController().popBackStack() }
+
+        viewModel.getRecords().observe(viewLifecycleOwner) { list ->
+            if (list.isNotEmpty()) {
+                adapter.setData(list)
+                binding.rvHistory.adapter = adapter
+            }
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/dev/unero/bukutabungan/ui/history/HistoryViewModel.kt
+++ app/src/main/java/dev/unero/bukutabungan/ui/history/HistoryViewModel.kt
@@ -1,7 +1,12 @@
 package dev.unero.bukutabungan.ui.history
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import dev.unero.bukutabungan.data.model.Record
+import dev.unero.bukutabungan.domain.repository.RecordRepository
 
-class HistoryViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class HistoryViewModel(
+    private val repository: RecordRepository
+) : ViewModel() {
+    fun getRecords(): LiveData<List<Record>> = repository.getAllRecord()
 }
