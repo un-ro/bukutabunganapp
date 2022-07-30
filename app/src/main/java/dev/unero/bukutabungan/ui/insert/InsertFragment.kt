1293bec0f42a647f257b4cea6b52ca3c4a5e1a5d
Unero <ramadhanbhg@gmail.com>
2022-07-30 16:08:42 +0700

    complete Insert function

--- app/src/main/java/dev/unero/bukutabungan/ui/insert/InsertFragment.kt
+++ app/src/main/java/dev/unero/bukutabungan/ui/insert/InsertFragment.kt
@@ -1,5 +1,6 @@
 package dev.unero.bukutabungan.ui.insert
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,13 +10,29 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import dev.unero.bukutabungan.R
+import dev.unero.bukutabungan.data.model.Record
 import dev.unero.bukutabungan.databinding.FragmentInsertBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 class InsertFragment : Fragment() {
     private var _binding: FragmentInsertBinding? = null
     private val binding get() = _binding as FragmentInsertBinding
-
     private val args by navArgs<InsertFragmentArgs>()
+
+    private val viewModel: InsertViewModel by viewModel()
+
+    private val calendar by lazy { Calendar.getInstance() }
+
+    private val datePicker by lazy {
+        DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateLabel(calendar)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,7 +49,44 @@ class InsertFragment : Fragment() {
             title = getCurrentTitle()
             setTitleTextColor(getTitleColor())
         }
+
+        binding.etDate.setOnClickListener {
+            DatePickerDialog(
+                requireContext(),
+                datePicker,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+
+        binding.btnSave.setOnClickListener {
+            if (binding.etDate.text.isEmpty())
+                binding.etDate.error = getString(R.string.error_empty_field)
+            else if (binding.etAmount.text.toString().isEmpty())
+                binding.etAmount.error = getString(R.string.error_empty_field)
+            else if (binding.etDescription.text.toString().isEmpty())
+                binding.etDescription.error = getString(R.string.error_empty_field)
+            else {
+                viewModel.insertRecord(newRecord())
+                findNavController().popBackStack()
+            }
+        }
     }
+
+    private fun updateLabel(calendar: Calendar) {
+        val sdf = SimpleDateFormat(localePattern, Locale.getDefault())
+        binding.etDate.setText(sdf.format(calendar.time))
+    }
+
+    private fun newRecord(): Record =
+        Record(
+            0,
+            binding.etAmount.text.toString().toInt(),
+            binding.etDescription.text.toString(),
+            binding.etDate.text.toString(),
+            args.isIncome
+        )
 
     private fun getCurrentTitle(): String =
         if (args.isIncome) getString(R.string.add_income)
@@ -45,5 +99,9 @@ class InsertFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        private const val localePattern = "dd-MM-yyyy"
     }
 }
--- app/src/main/java/dev/unero/bukutabungan/ui/insert/InsertViewModel.kt
+++ app/src/main/java/dev/unero/bukutabungan/ui/insert/InsertViewModel.kt
@@ -1,7 +1,16 @@
 package dev.unero.bukutabungan.ui.insert
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.unero.bukutabungan.data.model.Record
+import dev.unero.bukutabungan.domain.repository.RecordRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class InsertViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class InsertViewModel(
+    private val repository: RecordRepository
+) : ViewModel() {
+    fun insertRecord(record: Record) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insertRecord(record)
+    }
 }
